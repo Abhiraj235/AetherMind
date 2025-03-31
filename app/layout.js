31012ab1947e002be53aebe73c21cf5851fcb9f7
Abhiraj235 <abhishekrajwar8445@gmail.com>
2025-03-31 13:18:59 +0530

    final

---
+++ app/layout.js
@@ -0,0 +1,77 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AI Career Coach",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+
+            <footer className="bg-gray-800 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-300">
+                <p>Made with ❤️ by Alphas</p>
+                <p className="mt-2 text-sm">
+                  AI Career Coach - Helping you navigate the future of AI careers
+                </p>
+                <ul className="flex justify-center mt-4 space-x-4">
+                  <li>
+                    <a
+                      href="#"
+                      className="text-gray-400 hover:text-gray-200 transition duration-300"
+                    >
+                      Privacy Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="text-gray-400 hover:text-gray-200 transition duration-300"
+                    >
+                      Terms of Service
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="text-gray-400 hover:text-gray-200 transition duration-300"
+                    >
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
